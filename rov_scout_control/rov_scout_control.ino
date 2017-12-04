2ba0d9229af7b8484e4018ea98bcab95fdc6b722
zinjkov <zinjkov.su@gmail.com>
2017-12-04 17:40:51 +1000

    Добавьте файлы проекта.

---
+++ rov_scout_control/rov_scout_control.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		rov_scout_control.ino
+ Created:	04.12.2017 17:40:48
+ Author:	zinjk
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
